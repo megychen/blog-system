dbe2a3ed953be661cb2facef38ac9b978418b9f8
megy <megy@megys-MacBook-Pro.local>
2017-05-28 00:34:08 +0800

    create blog title after sign_up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,16 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  after_create :generate_blog_title
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_one :blog
   validates :user_name, uniqueness: true
 
   mount_uploader :avatar, AvatarUploader
+
+  def generate_blog_title
+    Blog.create(title: self.user_name, user_id: self.id)
+  end
 end
