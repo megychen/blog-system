b10326abb042b700380737b92a38ededc894a191
megy <megy@megys-MacBook-Pro.local>
2017-05-29 17:42:14 +0800

    change disqus short-name

--- config/initializers/disqus_rails.rb
+++ config/initializers/disqus_rails.rb
@@ -1,5 +1,5 @@
 DisqusRails.setup do |config|
-  config::SHORT_NAME = "disqus_ulfNLW5aaR"
+  config::SHORT_NAME = "myblogger-2"
   config::SECRET_KEY = "T4wgKm9mDTXQNrxNrhy8mrYzx3plVOtIlXeebUxTRvZty0awOLsu6OeGWbOD9uQT"
   config::PUBLIC_KEY = "9TQaYo86fb61JDiDJe6xaGH1AKTS4F4TBG5gz8A6W6UHr5R2mYkAuGHXvQsSQkVa"
   config::ACCESS_TOKEN = "b264ca9078c44fb6898b928eeb22661e"
