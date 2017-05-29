057c8ff98330a86f0ae9619d0424357432db1d6e
megy <megy@megys-MacBook-Pro.local>
2017-05-29 16:31:06 +0800

    and archives page and category filter

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -5,6 +5,16 @@ class BlogsController < ApplicationController
 
   def show
     @blog = Blog.find(params[:id])
-    @posts = @blog.posts.where(:status => "public").paginate(:page => params[:page], :per_page => 5)
+    @posts = @blog.posts.where(:status => "public").recent.paginate(:page => params[:page], :per_page => 5)
+  end
+
+  def archives
+    @blog = Blog.find(params[:id])
+    if params[:category].present?
+      @category_id = Category.find_by(name: params[:category]).id
+      @posts = @blog.posts.where(:status => "public", category_id: @category_id).recent
+    else
+      @posts = @blog.posts.where(:status => "public").recent
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :registrations => "users/registrations" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "blogs#index"
-  resources :blogs, only: [:index, :show]
+  resources :blogs, only: [:index, :show] do
+    member do
+      get :archives
+    end
+  end
   resources :posts
 
   get "/dashboard"  => "dashboard#index", :as => :dashboard
