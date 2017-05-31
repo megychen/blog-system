dbfde48e0486213ad51613ea59ec940b68a8e19b
megy <megy@megys-MacBook-Pro.local>
2017-05-31 10:26:51 +0800

    finish signup/signin/logout testing

--- test/controllers/account/blogs_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class Account::BlogsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/controllers/account/categories_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class Account::CategoriesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
