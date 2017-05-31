b715585661a093151bca41193b8e597a6700e105
megy <megy@megys-MacBook-Pro.local>
2017-06-01 01:37:54 +0800

    implement admin::posts controller

---
+++ app/controllers/admin/posts_controller.rb
@@ -0,0 +1,40 @@
+class Admin::PostsController < AdminController
+  before_action :authenticate_user!
+  before_action :require_admin!
+  before_action :find_admin_post, :except => [:index]
+  layout "admin"
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to admin_posts_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+    flash[:alert] = "Post deleted"
+    redirect_to admin_posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :description, :category_id)
+  end
+
+  def find_admin_post
+    @post = Post.find(params[:id])
+  end
+end
